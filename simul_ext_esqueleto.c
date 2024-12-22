b1dfe5ecf80944bace1a8b6c858d0cbf2346ef3a
Enrique Iglesias <ike.umafi@gmail.com>
2024-12-22 20:56:44 +0100

    The functions corresponding to the remove and rename commands have been implemented, and while they are difficult to test since there are issues reading particion.bin (seems like it is empty), they seem to function correctly. The ComprobarComando() function was changed, adding the functionality of splitting the command so that we can get the actual command (orden) as well as the arguments if there are any. This introduced a small error that will be fixed as soon as possible, the error being that if you introduce a command with no arguments, you must add an empty space at the end for the script to recognize it.

--- simul_ext_esqueleto.c
+++ simul_ext_esqueleto.c
@@ -6,7 +6,7 @@
 #define LONGITUD_COMANDO 100
 
 void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps);
-int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2);
+int ComprobarComando(char *strcomando, char **orden, char **argumento1, char **argumento2, char *token);
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup);
 int BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, char *nombre);
 void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos);
@@ -23,14 +23,18 @@ void GrabarByteMaps(EXT_BYTE_MAPS *ext_bytemaps, FILE *fich);
 void GrabarSuperBloque(EXT_SIMPLE_SUPERBLOCK *ext_superblock, FILE *fich);
 void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
 
-//NEW FUNCTIONS NOT ORIGINALY IN THE FILE PROVIDED
-
 	 int main(){
 	 int i,j;
 	 unsigned long int m;
-	 	char *orden;
-	 	char *argumento1;
-	 	char *argumento2;
+	 char *orden = (char*)calloc(LONGITUD_COMANDO, sizeof(char));
+	 char *argumento1 = (char*)calloc(LONGITUD_COMANDO, sizeof(char));
+	 char *argumento2 = (char*)calloc(LONGITUD_COMANDO, sizeof(char));
+	 char *token = (char*)calloc(LONGITUD_COMANDO, sizeof(char));
+	 if (orden == NULL || argumento1 == NULL || argumento2 == NULL || token == NULL)
+	 {
+	  	 printf("Error allocating memory\n");
+	  	 exit(0);
+	 }
      EXT_SIMPLE_SUPERBLOCK ext_superblock;
      EXT_BYTE_MAPS ext_bytemaps;
      EXT_BLQ_INODOS ext_blq_inodos;
@@ -45,8 +49,7 @@ void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
      fent = fopen("particion.bin","r+b");
      fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);
 
-
-	 	memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
+	 memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
      memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], MAX_FICHEROS);
      memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
      memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
@@ -65,14 +68,15 @@ void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
 		 fflush(stdin);
 		 fgets(comando, LONGITUD_COMANDO, stdin);
 	     //printf("I am 1 \n");
-	     orden = strtok(comando, "\n");
-	     /* printf("Comando: %s\n", comando);
-	     printf("Orden: %s\n", orden);
-	     printf("Length of comando: %d\n", strlen(comando));
-	     printf("Length of orden: %d\n", strlen(orden));
-	     printf("Length of \"salir\": %d\n", strlen("salir"));
-	     printf("strcmp between comando and orden: %d\n", strcmp(comando, orden)); */
-		 } while (ComprobarComando(comando,orden,argumento1,argumento2) !=0);
+	     //orden = strtok(comando, "\n");
+	     //SplitCommand(comando, orden, argumento1, argumento2, token);
+	     //printf("Comando: %s\n", comando);
+	     //printf("Orden: %s\n", orden);
+	     //printf("Length of comando: %d\n", strlen(comando));
+	     //printf("Length of orden: %d\n", strlen(orden));
+	     //printf("Length of \"salir\": %d\n", strlen("salir"));
+	     //printf("strcmp between comando and orden: %d\n", strcmp(comando, orden));
+		 } while (ComprobarComando(comando,&orden,&argumento1,&argumento2, token) !=0);
 
          // Escritura de metadatos en comandos rename, remove, copy
          /* GrabarByteMaps(&ext_bytemaps,fent);
@@ -85,6 +89,7 @@ void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
          //faltan los datos y cerrar
 
      	 //printf("I am 2 \n");
+     	 printf("Orden: %s\n", orden);
 			if(strcmp(orden, "salir") == 0) {
 				//GrabarDatos(memdatos,fent);
 				fclose(fent);
@@ -99,67 +104,72 @@ void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
 			}else if(strcmp(orden, "dir") == 0) {
 				Directorio(&directorio[MAX_FICHEROS],&ext_blq_inodos);
 				printf("dir command executed\n");
-			}else if(strcmp(orden, "rename\n") == 0) {
+			}else if(strcmp(orden, "rename") == 0) {
+				Renombrar(directorio, &ext_blq_inodos, argumento1, argumento2);
 				printf("rename command executed\n");
 			}else if(strcmp(orden, "print") == 0) {
 				printf("print command executed\n");
 			}else if(strcmp(orden, "remove") == 0) {
+				Borrar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, argumento1, fent);
 				printf("remove command executed\n");
 			}else if(strcmp(orden, "copy") == 0) {
-				printf("Give me a number in decimal.\n");
-				unsigned int answer;
-				scanf("%d",&answer);
 				printf("copy command executed\n");
 			}
-     	//switch (orden) {
-	        // case "salir":
-	         //	GrabarDatos(&memdatos,fent);
-         	//	fclose(fent);
-         	//	return 0;
-         	//case "info":
-         	//	void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup);
-
-
-         	//	break;
-         	//case "bytemaps":
-         	//	void Printbytemaps();
-
-         	//	break;
-         //	case "dir":
-         	//	Directorio(&directorio,&ext_blq_inodos);
-         	//	break;
-         	//case "rename":
-         	//	break;
-         	//case "print":
-         	//	break;
-         	//case"remove":
-         	//	break;
-         	//case "copy":
-         	//	break;
-         //	default:
-         //		printf("invalid command[info,bytemaps,dir,rename,print,remove,copy]");
-         //	break;
-         }//
-
-
+         }
      }
 
+int ComprobarComando(char *strcomando, char **orden, char **argumento1, char **argumento2, char* token){
+	 	//---------COMMAND SPLITTING---------//
+	 	token = strtok(strcomando, " ");
 
-int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2){
-	if (orden == NULL) {
+	 	for (int i = 0; i < 3; i++)
+	 	{
+	 		if (token != NULL)
+	 		{
+	 			if (i == 0)
+	 			{
+	 				*orden = token;
+	 			}
+
+	 			if (i == 1)
+	 			{
+	 				*argumento1 = token;
+	 			}
+
+	 			if (i == 2)
+	 			{
+	 				*argumento2 = token;
+	 			}
+	 			//printf("TOKEN: %s\n", token);
+	 			token = strtok(NULL, " ");
+
+	 		}
+	 	}
+
+	 	/* printf("Comando: %s\n",strcomando);
+	 	printf("Orden: %s\n",*orden);
+	 	printf("Argumento 1: %s\n",*argumento1);
+	 	printf("Argumento 2: %s\n",*argumento2);
+	 	printf("Length of comando: %d\n", strlen(strcomando));
+	 	printf("Length of orden: %d\n", strlen(*orden));
+	 	printf("Length of \"dir\": %d\n", strlen("dir")); */
+	 	//---------END COMMAND SPLITTING-----------//
+
+
+	if (*orden == NULL) {
 		printf("No command entered.\n");
 		return -1; // Invalid command
 	}
 
 	// Check for valid commands
-	if (strcmp(orden, "salir") == 0 || strcmp(orden, "info") == 0 ||
-		strcmp(orden, "bytemaps") == 0 || strcmp(orden, "dir") == 0 ||
-		strcmp(orden, "rename") == 0 || strcmp(orden, "print") == 0 ||
-		strcmp(orden, "remove") == 0 || strcmp(orden, "copy") == 0) {
+	if (strcmp(*orden, "salir") == 0 || strcmp(*orden, "info") == 0 ||
+		strcmp(*orden, "bytemaps") == 0 || strcmp(*orden, "dir") == 0 ||
+		strcmp(*orden, "rename") == 0 || strcmp(*orden, "print") == 0 ||
+		strcmp(*orden, "remove") == 0 || strcmp(*orden, "copy") == 0) {
 		return 0; // Command is valid
 		}
 
-	printf("Invalid command: %s\n", orden);
+	printf("Invalid command: %s\n", *orden);
 	return -1; // Command not recognized
 }
 
@@ -191,6 +201,8 @@ void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup) {
 }
 int Imprimir(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_DATOS *memdatos, char *nombre) {
 
+	 	// Placeholder until the function is implemented
+	 	return 0;
 }
 void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos) {
 	 	printf("Directory contents:\n");
@@ -212,15 +224,115 @@ void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos) {
 	 }
 
 int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_BYTE_MAPS *ext_bytemaps,
-			EXT_SIMPLE_SUPERBLOCK *ext_superblock, char *nombre,  FILE *fich)
+			EXT_SIMPLE_SUPERBLOCK *ext_superblock, char *nombre, FILE *fich)
 {
-	 	//Mark one i-node as free in the superblock, since we are getting rid of one file.
+	 	int found = -1;
+
+	 	// Search for the file in the directory (once found, we break the loop)
+	 	for (int i = 0; i < MAX_FICHEROS; i++)
+	 	{
+	 		// This happens when the file we want to delete is found (not NULL inode and the name matches)
+	 		if (directorio[i].dir_inodo != NULL_INODO && strcmp(directorio[i].dir_nfich, nombre) == 0)
+	 		{
+	 			// Found takes the value of the index where the file we want to delete is
+	 			found = i;
+	 			break;
+			}
+	 	}
+
+	 	// If the file is not found, return an error telling the user that the file either doesn't exist or wasn't found
+	 	if (found == -1)
+	 	{
+	 		printf("Error: File '%s' doesn't exist or was not found.\n", nombre);
+	 		return -1;
+	 	}
+
+	 	// Store the inode index in an int variable for use below
+	 	// (Technically not neccessary, but makes the code look a lot cleaner)
+	 	int inode_index = directorio[found].dir_inodo;
+
+	 	// Free the data blocks associated with the file
+	 	for (int i = 0; i < MAX_NUMS_BLOQUE_INODO; i++)
+	 	{
+	 		if (inodos->blq_inodos[inode_index].i_nbloque[i] != NULL_BLOQUE)
+	 		{
+	 			// We store the block index in an int variable for use below
+	 			// (Technically not neccessary, but makes the code look a lot cleaner)
+	 			int block_index = inodos->blq_inodos[inode_index].i_nbloque[i];
+	 			// Mark block as free
+	 			ext_bytemaps->bmap_bloques[block_index] = 0;
+	 			// Increment free block count
+	 			ext_superblock->s_free_blocks_count++;
+	 			// Clear block reference
+	 			inodos->blq_inodos[inode_index].i_nbloque[i] = NULL_BLOQUE;
+	 		}
+	 	}
+
+	 	// Free the inode and increment the free inode count in the superblock to reflect this change
+	 	ext_bytemaps->bmap_inodos[inode_index] = 0;
 	 	ext_superblock->s_free_inodes_count++;
+
+	 	// Clear the directory entry
+	 	directorio[found].dir_inodo = NULL_INODO;
+	 	memset(directorio[found].dir_nfich, 0, LEN_NFICH);
+
+	 	// Tell the user that the file was deleted and return 0 to signal correct execution.
+	 	printf("File \"%s\" deleted.\n", nombre);
 	 	return 0;
 }
+
 
 
 void GrabarDatos(EXT_DATOS *memdatos, FILE *fich)
 {
 	 	//TODO: Implement this function so that no errors arise
 }
+
+int Renombrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, char *nombreantiguo, char *nombrenuevo)
+{
+	 	int foundOld = -1;
+	 	int foundNew = -1;
+
+	 	// Search for the old name and check if the new name already exists
+	 	for (int i = 0; i < MAX_FICHEROS; i++)
+	 	{
+	 		if (directorio[i].dir_inodo != NULL_INODO)
+	 		{
+	 			// This happens when the file to be renamed is found
+	 			if (strcmp(directorio[i].dir_nfich, nombreantiguo) == 0)
+	 			{
+	 				// Found old takes the value of the index where the file we want to rename is
+	 				foundOld = i;
+	 			}
+	 			// This happens when a file is found with the new name already in use
+	 			if (strcmp(directorio[i].dir_nfich, nombrenuevo) == 0)
+	 			{
+	 				// Found new takes the value of the index where there is a file with the new name.
+	 				foundNew = i;
+	 			}
+	 		}
+	 	}
+
+	 	// If the old file is not found, return an error and tell the user that the file doesn't exist
+	 	if (foundOld == -1) {
+	 		printf("Error: File \"%s\" doesn't exist or was not found.\n", nombreantiguo);
+	 		return -1;
+	 	}
+
+	 	// If the new name already exists, return an error and tell the user that that name is already taken
+	 	if (foundNew != -1) {
+	 		printf("Error: File \"%s\" already exists.\n", nombrenuevo);
+	 		return -1;
+	 	}
+
+	 	// Rename the file
+	 	// (when specifying the amount of chars to be copied, we add -1 because the count starts from 0, not 1)
+	 	strncpy(directorio[foundOld].dir_nfich, nombrenuevo, LEN_NFICH - 1);
+	 	// Make sure that the last char is the "string end" char (VERY IMPORTANT, IF NOT DONE THERE CAN BE MEMORY ISSUES)
+	 	directorio[foundOld].dir_nfich[LEN_NFICH - 1] = '\0';
+
+	 	//File renamed, we return 0 to signal that everything went well
+	 	printf("File \"%s\" renamed to \"%s\".\n", nombreantiguo, nombrenuevo);
+	 	return 0;
+}
+
